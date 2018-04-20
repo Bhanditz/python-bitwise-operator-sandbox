db921d207deee718f6cccf23d834688f71503f74
Richard Shea <rshea@thecubagroup.com>
2018-04-20 21:50:44 +1200

    Tidy up

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bitwise Operations in Python
+This is (very) mini demonstration of how you may use the bitwise operators in Python.
+
+Tested in Python 2.7.
+
+## TODO
+- The other operators
+- Pick up the input values from the command line
--- bitwisedemo.py
+++ bitwisedemo.py
@@ -1,9 +1,5 @@
 def masktest(lhs, rhs):
     
-    #lhstext = "The LHS is {} - the binary equivalent of which is : {}"
-    #rhstext = "The RHS is {} - the binary equivalent of which is : {}"
-    #print('{:>90}'.format(lhstext.format(lhs, bin(lhs))))
-    #print('{:>30}'.format('right aligned'))
     print("")
     print("====================================================================")
     print("LOOKING AT {} and {} ".format(lhs, rhs))
@@ -12,21 +8,14 @@ def masktest(lhs, rhs):
     print("{:>90} {:08b}".format(s, lhs ))
     s = "The RHS is {} - the binary equivalent of which is : ".format(rhs) 
     print("{:>90} {:08b}".format(s, rhs ))
-    '''
-    print("The LHS is {} - the binary equivalent of which is : {:08b}".format(lhs, lhs)) 
-    print("The RHS is {} - the binary equivalent of which is : {:08b}".format(rhs, rhs)) 
-    '''
     print("")
 
     s = "When you 'bitwise and' the two values you get : {} - the binary equivalent of which : ".format(lhs & rhs)
     print("{:>90} {:08b}".format(s, lhs & rhs))
-    #print("When you 'bitwise and' the two values you get : {} - the binary equivalent of which : {:08b}".format(lhs & rhs, lhs & rhs))
 
     s = "When you 'bitwise or ' the two values you get : {} - the binary equivalent of which : ".format(lhs | rhs)
     print("{:>90} {:08b}".format(s, lhs | rhs))
 
-    #print("When you 'bitwise or ' the two values you get : {} - the binary equivalent of which : {:08b}".format(lhs | rhs, lhs | rhs))
-    #print("When you 'bitwise xor' the two values you get : {} - the binary equivalent of which : {:08b}".format(lhs ^ rhs, lhs ^ rhs))
 
     s = "When you 'bitwise xor' the two values you get : {} - the binary equivalent of which : ".format(lhs ^ rhs)
     print("{:>90} {:08b}".format(s, lhs ^ rhs))
@@ -34,7 +23,6 @@ def masktest(lhs, rhs):
     print("")
     print("====================================================================")
     print("")
-'{:<60} {}'.format('## execute', 'Execute an IRC command')
 
 if __name__ == "__main__":
     masktest(1, 3)
